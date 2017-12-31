b28b01b9a4b55222ce71d7fea7fb614486b92c84
Nasser <nasserbinshabeeb@mail.weber.edu>
2017-12-31 15:45:11 -0700

    Add project files.

---
+++ QSharpApplication1/Driver.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Quantum.QSharpApplication1
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Bel1();
+            Bel2();
+
+        }
+
+         static void Bel2()
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                // Try initial values
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+                foreach (Result initial in initials)
+                {
+                    var res = BellTest2.Run(sim, 1000, initial).Result;
+                    var (numZeros, numOnes, agree) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agree,-4}");
+                }
+            }
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+
+
+         static void Bel1()
+        {
+            using (var sim = new QuantumSimulator())
+            {
+                // Try initial values
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+                foreach (Result initial in initials)
+                {
+                    var res = BellTest.Run(sim, 1000, initial).Result;
+                    var (numZeros, numOnes) = res;
+                    System.Console.WriteLine(
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                }
+            }
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
+        }
+    }
+}
